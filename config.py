8d864f13a769ff6a6d2f6a3b23fe83eb618e7675
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2022-12-03 00:18:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,16 @@ def is_enabled(value, default):
         return default
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID", "13335517"))
+API_HASH = os.environ.get("API_HASH", "76b6a02d5f039628ac07e72108638c1a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5515274385:AAGEp4AqZGJrSFpnKAszJoQRRaLD6ms2oxs")
 DROPLINK_API = os.environ.get("DROPLINK_API")
 MDISK_API = os.environ.get("MDISK_API")
 ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertor")
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Ravi:mdisk@cluster0.53vglyi.mongodb.net/?retryWrites=true&w=majority
+
+")
 WEBSITE = os.environ.get('WEBSITE')
 
 #  Optionnal variables
@@ -25,7 +27,7 @@ EXCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("EXCLUDE_DOMAIN").split(
 CHANNELS = is_enabled((os.environ.get('CHANNELS', "True")), True)
 CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")) if os.environ.get("CHANNEL_ID") else []
 FORWARD_MESSAGE = is_enabled((os.environ.get('FORWARD_MESSAGE', "False")), False)
-SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://t.me/potter_00")
+SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://github.com/Ps705112/URL-Shortener-V2")
 USERNAME = os.environ.get("USERNAME", None)
 HEADER_TEXT = os.environ.get("HEADER_TEXT", '')
 FOOTER_TEXT = os.environ.get("FOOTER_TEXT", '')
